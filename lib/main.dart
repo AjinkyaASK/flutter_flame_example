f126c307b183b7825622acf83baa42f105f5d190
Ajinkya Karanjikar <ajinkya.karanjikar@gmail.com>
2022-12-14 10:26:04 +0530

    Ongoing Enemy Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
@@ -32,14 +30,18 @@ class WoodRunner extends FlameGame with TapDetector, HasCollisionDetection {
     debugMode = true;
   }
 
-  late final Component floor;
-  late final Component background;
+  late final ParallaxComponent floor;
+  late final ParallaxComponent background;
   late final PlayerCharacter player;
+  late final FlyingEyeEnemy flyingEyeEnemy;
+  late final SkeletonEnemy skeletonEnemy;
+
   Vector2 viewportSize = Vector2.zero();
   double floorHeight = 0.0;
 
-  final double playerSpeed = 80;
+  double playerSpeed = 80;
   final double playerSizeFactor = 0.2125;
+  final double enemySizeFactor = 0.2125;
 
   Future<void> initComponents() async {
     background = ParallaxComponent(
@@ -91,9 +93,37 @@ class WoodRunner extends FlameGame with TapDetector, HasCollisionDetection {
       position: Vector2(0, viewportSize.y - floorHeight),
     );
     player = PlayerCharacter(
+      speed: playerSpeed,
       size: Vector2.all(viewportSize.y * playerSizeFactor),
       position: Vector2(viewportSize.x * 0.075,
           viewportSize.y - floorHeight - (viewportSize.y * playerSizeFactor)),
+      onHurt: () {
+        playerSpeed = 0;
+        background.parallax!.baseVelocity = Vector2(playerSpeed * 0.8, 0);
+        floor.parallax!.baseVelocity = Vector2(playerSpeed, 0);
+      },
+      onRun: () {
+        playerSpeed = 80;
+        background.parallax!.baseVelocity = Vector2(playerSpeed * 0.8, 0);
+        floor.parallax!.baseVelocity = Vector2(playerSpeed, 0);
+      },
+      onJump: () {},
+      onAttack: () {},
+      onDie: () {
+        playerSpeed = 0;
+        background.parallax!.baseVelocity = Vector2(playerSpeed * 0.8, 0);
+        floor.parallax!.baseVelocity = Vector2(playerSpeed, 0);
+      },
+    );
+    flyingEyeEnemy = await FlyingEyeEnemy.create(
+      size: Vector2.all(viewportSize.y * enemySizeFactor),
+      position: Vector2(viewportSize.x * 1.5,
+          viewportSize.y - floorHeight - (viewportSize.y * enemySizeFactor)),
+    );
+    skeletonEnemy = await SkeletonEnemy.create(
+      size: Vector2.all(viewportSize.y * enemySizeFactor),
+      position: Vector2(viewportSize.x * 1,
+          viewportSize.y - floorHeight - (viewportSize.y * enemySizeFactor)),
     );
     await player.init();
   }
@@ -112,6 +142,8 @@ class WoodRunner extends FlameGame with TapDetector, HasCollisionDetection {
     add(background);
     add(floor);
     add(player);
+    add(flyingEyeEnemy);
+    add(skeletonEnemy);
 
     return super.onLoad();
   }
@@ -123,12 +155,19 @@ class WoodRunner extends FlameGame with TapDetector, HasCollisionDetection {
   }
 }
 
+enum PlayerState { idle, run, jump, attack, hurt, die }
+
 class PlayerCharacter extends SpriteAnimationComponent with CollisionCallbacks {
   PlayerCharacter({
     super.size,
     super.position,
     this.speed = 125 * 8,
     this.jumpHeight = 80,
+    this.onHurt,
+    this.onRun,
+    this.onJump,
+    this.onAttack,
+    this.onDie,
   }) {
     basePosition = position.clone();
   }
@@ -138,16 +177,22 @@ class PlayerCharacter extends SpriteAnimationComponent with CollisionCallbacks {
   late final SpriteSheet spriteSheet;
   late final SpriteAnimation idleAnimation;
   late final SpriteAnimation runAnimation;
-  late final SpriteAnimation attackAnimation;
 
   final double speed; //Milliseconds to complete on run animation
   final double jumpHeight;
+  final void Function()? onHurt;
+  final void Function()? onRun;
+  final void Function()? onJump;
+  final void Function()? onAttack;
+  final void Function()? onDie;
+
+  PlayerState state = PlayerState.idle;
 
   Future<void> init() async {
     spriteSheet = SpriteSheet.fromColumnsAndRows(
       image: await Flame.images.load('character/char_blue.png'),
       columns: 8,
-      rows: 7,
+      rows: 8,
     );
 
     idleAnimation = spriteSheet.createAnimation(
@@ -174,13 +219,41 @@ class PlayerCharacter extends SpriteAnimationComponent with CollisionCallbacks {
     return super.onLoad();
   }
 
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    if (state != PlayerState.hurt) {
+      if (other is Enemy) {
+        hurt();
+      }
+    }
+    super.onCollision(intersectionPoints, other);
+  }
+
   void idle() {
+    state = PlayerState.idle;
     animation = idleAnimation;
   }
 
   void run() {
+    if (onRun != null) {
+      onRun!();
+    }
+    state = PlayerState.run;
     position = basePosition.clone();
     animation = runAnimation;
+  }
+
+  void hurt() {
+    if (onHurt != null) {
+      onHurt!();
+    }
+    state = PlayerState.hurt;
+    position = basePosition.clone();
+    animation = animation = spriteSheet.createAnimation(
+      row: 7,
+      stepTime: 0.125,
+      loop: false,
+    )..onComplete = run;
   }
 
   void jump() {
@@ -188,17 +261,23 @@ class PlayerCharacter extends SpriteAnimationComponent with CollisionCallbacks {
   }
 
   void _jumpUp() {
-    animation = spriteSheet.createAnimation(
-      row: 3,
-      stepTime: 0.125,
-      loop: false,
-    )
-      ..onFrame = (frame) {
-        final newY = basePosition.y - ((jumpHeight / 8) * (frame + 1));
-        y = newY >= (basePosition.y - jumpHeight) ? newY : basePosition.y;
-        // log('Jumping up - Height: $y');
+    if (state != PlayerState.jump) {
+      if (onJump != null) {
+        onJump!();
       }
-      ..onComplete = _jumpDown;
+      state = PlayerState.jump;
+      animation = spriteSheet.createAnimation(
+        row: 3,
+        stepTime: 0.125,
+        loop: false,
+      )
+        ..onFrame = (frame) {
+          final newY = basePosition.y - ((jumpHeight / 8) * (frame + 1));
+          y = newY >= (basePosition.y - jumpHeight) ? newY : basePosition.y;
+          // log('Jumping up - Height: $y');
+        }
+        ..onComplete = _jumpDown;
+    }
   }
 
   void _jumpDown() {
@@ -217,10 +296,188 @@ class PlayerCharacter extends SpriteAnimationComponent with CollisionCallbacks {
   }
 
   void die() {
-    animation = spriteSheet.createAnimation(
-      row: 5,
-      stepTime: 0.175,
-      loop: false,
+    if (state != PlayerState.die) {
+      if (onDie != null) {
+        onDie!();
+      }
+      state = PlayerState.die;
+      animation = spriteSheet.createAnimation(
+        row: 5,
+        stepTime: 0.175,
+        loop: false,
+      );
+    }
+  }
+}
+
+class FlyingEyeEnemy extends Enemy {
+  FlyingEyeEnemy({
+    super.size,
+    super.position,
+    super.speed,
+    required super.idleSpriteSheet,
+    required super.runSpriteSheet,
+    required super.attackSpriteSheet,
+  }) : super(flying: true);
+
+  static Future<FlyingEyeEnemy> create({
+    required Vector2 size,
+    required Vector2 position,
+    double speed = 125 * 8,
+  }) async {
+    final object = FlyingEyeEnemy(
+      size: size,
+      speed: speed,
+      position: position,
+      idleSpriteSheet: SpriteSheet.fromColumnsAndRows(
+        image: await Flame.images.load('character/enemy/FlyingEye/Flight.png'),
+        columns: 8,
+        rows: 1,
+      ),
+      runSpriteSheet: SpriteSheet.fromColumnsAndRows(
+        image: await Flame.images.load('character/enemy/FlyingEye/Flight.png'),
+        columns: 8,
+        rows: 1,
+      ),
+      attackSpriteSheet: SpriteSheet.fromColumnsAndRows(
+        image: await Flame.images.load('character/enemy/FlyingEye/Attack.png'),
+        columns: 8,
+        rows: 1,
+      ),
     );
+    await object.init();
+    return object;
+  }
+}
+
+class SkeletonEnemy extends Enemy {
+  SkeletonEnemy({
+    super.size,
+    super.position,
+    super.speed,
+    required super.idleSpriteSheet,
+    required super.runSpriteSheet,
+    required super.attackSpriteSheet,
+  }) : super(running: true);
+
+  static Future<SkeletonEnemy> create({
+    required Vector2 size,
+    required Vector2 position,
+    double speed = 125 * 8,
+  }) async {
+    final object = SkeletonEnemy(
+      size: size,
+      speed: speed,
+      position: position,
+      idleSpriteSheet: SpriteSheet.fromColumnsAndRows(
+        image: await Flame.images.load('character/enemy/Skeleton/Idle.png'),
+        columns: 4,
+        rows: 1,
+      ),
+      runSpriteSheet: SpriteSheet.fromColumnsAndRows(
+        image: await Flame.images.load('character/enemy/Skeleton/Walk.png'),
+        columns: 4,
+        rows: 1,
+      ),
+      attackSpriteSheet: SpriteSheet.fromColumnsAndRows(
+        image: await Flame.images.load('character/enemy/Skeleton/Attack.png'),
+        columns: 8,
+        rows: 1,
+      ),
+    );
+    await object.init();
+    return object;
+  }
+}
+
+enum EnemyState { idle, run, attack, die }
+
+abstract class Enemy extends SpriteAnimationComponent with CollisionCallbacks {
+  Enemy({
+    super.size,
+    super.position,
+    this.speed = 125 * 8,
+    this.flying = false,
+    this.running = false,
+    required this.idleSpriteSheet,
+    required this.runSpriteSheet,
+    required this.attackSpriteSheet,
+  }) {
+    basePosition = position.clone();
+  }
+
+  late final Vector2 basePosition;
+
+  final double speed; //Milliseconds to complete on run animation
+  final bool flying;
+  final bool running;
+
+  late final SpriteSheet idleSpriteSheet;
+  late final SpriteSheet runSpriteSheet;
+  late final SpriteSheet attackSpriteSheet;
+
+  late final SpriteAnimation idleAnimation;
+  late final SpriteAnimation runAnimation;
+
+  EnemyState state = EnemyState.idle;
+
+  Future<void> init() async {
+    idleAnimation = idleSpriteSheet.createAnimation(
+      row: 0,
+      stepTime: 0.125,
+    );
+    runAnimation = runSpriteSheet.createAnimation(
+      row: 0,
+      stepTime: (speed / idleSpriteSheet.columns) / 1000,
+    );
+    run();
+  }
+
+  @override
+  Future<void>? onLoad() {
+    add(RectangleHitbox(
+      position: Vector2(size.x * 0.25, size.y * 0.25),
+      size: Vector2(size.x * 0.5, size.y * 0.5),
+    ));
+    return super.onLoad();
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    attack();
+    super.onCollision(intersectionPoints, other);
+  }
+
+  @override
+  void update(double dt) {
+    if (running || flying) {
+      if (x > (size.x * -1)) {
+        x -= 1;
+      } else {
+        x = basePosition.x;
+      }
+    }
+    super.update(dt);
+  }
+
+  idle() {
+    state = EnemyState.idle;
+    animation = idleAnimation;
+  }
+
+  run() {
+    state = EnemyState.run;
+    animation = runAnimation;
+  }
+
+  attack() {
+    if (state != EnemyState.attack) {
+      state = EnemyState.attack;
+      animation = attackSpriteSheet.createAnimation(
+        row: 0,
+        stepTime: 0.125,
+        loop: false,
+      )..onComplete = run;
+    }
   }
 }
